5692312cbbf7ff02a183ab0582a4fe28a109976c
lh <537484111@qq.com>
2019-12-25 21:51:47 +0800

    JUnit测试

---
+++ src/test/java/Array/arrayTest.java
@@ -0,0 +1,22 @@
+package Array;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class arrayTest {
+
+	@Test
+	public void test() {
+		int[] arrayInt = new int[] {1,2,3};
+		System.out.print(Arrays.toString(arrayInt));
+	}
+	
+	@Test
+	public void test2() {
+		System.out.print("hi");
+	}
+
+}
